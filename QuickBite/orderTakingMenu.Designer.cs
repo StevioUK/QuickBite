0e0a74e1decf7abda5a68663ce65569bacb8c567
Stevio <59091187+StevioUK@users.noreply.github.com>
2023-05-26 21:53:38 +0100

    bunch of design changes

--- QuickBite/orderTakingMenu.Designer.cs
+++ QuickBite/orderTakingMenu.Designer.cs
@@ -30,15 +30,29 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(orderTakingMenu));
             headerBackground = new Panel();
-            listViewButton = new Button();
-            pointsText = new Label();
+            label1 = new Label();
             pointsLabel = new Label();
+            pointsText = new Label();
+            listViewButton = new Button();
+            label3 = new Label();
+            label2 = new Label();
+            label4 = new Label();
+            seasonalSpecialImage1 = new Button();
+            seasonalSpecialImage2 = new Button();
+            seasonalSpecialImage3 = new Button();
+            mealComboImage3 = new Button();
+            mealComboImage2 = new Button();
+            mealComboImage1 = new Button();
+            todayBargainImage3 = new Button();
+            todayBargainImage2 = new Button();
+            todayBargainImage1 = new Button();
             headerBackground.SuspendLayout();
             SuspendLayout();
             // 
             // headerBackground
             // 
             headerBackground.BackgroundImage = (Image)resources.GetObject("headerBackground.BackgroundImage");
+            headerBackground.Controls.Add(label1);
             headerBackground.Controls.Add(pointsLabel);
             headerBackground.Controls.Add(pointsText);
             headerBackground.Controls.Add(listViewButton);
@@ -47,6 +61,40 @@
             headerBackground.Name = "headerBackground";
             headerBackground.Size = new Size(524, 65);
             headerBackground.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("MV Boli", 24F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(94, 10);
+            label1.Name = "label1";
+            label1.Size = new Size(347, 41);
+            label1.TabIndex = 4;
+            label1.Text = "Welcome to Quickbite";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // pointsLabel
+            // 
+            pointsLabel.AutoSize = true;
+            pointsLabel.BackColor = Color.Transparent;
+            pointsLabel.Location = new Point(469, 31);
+            pointsLabel.Name = "pointsLabel";
+            pointsLabel.Size = new Size(40, 15);
+            pointsLabel.TabIndex = 3;
+            pointsLabel.Text = "Points";
+            pointsLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // pointsText
+            // 
+            pointsText.AutoSize = true;
+            pointsText.BackColor = Color.Transparent;
+            pointsText.Location = new Point(469, 12);
+            pointsText.Name = "pointsText";
+            pointsText.Size = new Size(43, 15);
+            pointsText.TabIndex = 2;
+            pointsText.Text = "564564";
+            pointsText.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // listViewButton
             // 
@@ -62,39 +110,148 @@
             listViewButton.TabIndex = 1;
             listViewButton.UseVisualStyleBackColor = false;
             // 
-            // pointsText
+            // label3
             // 
-            pointsText.AutoSize = true;
-            pointsText.BackColor = Color.Transparent;
-            pointsText.Location = new Point(469, 12);
-            pointsText.Name = "pointsText";
-            pointsText.Size = new Size(43, 15);
-            pointsText.TabIndex = 2;
-            pointsText.Text = "564564";
-            pointsText.TextAlign = ContentAlignment.MiddleCenter;
+            label3.AutoSize = true;
+            label3.BackColor = Color.Transparent;
+            label3.Font = new Font("MV Boli", 24F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(12, 85);
+            label3.Name = "label3";
+            label3.Size = new Size(271, 41);
+            label3.TabIndex = 5;
+            label3.Text = "Seasonal Specials";
+            label3.TextAlign = ContentAlignment.MiddleCenter;
             // 
-            // pointsLabel
+            // label2
             // 
-            pointsLabel.AutoSize = true;
-            pointsLabel.BackColor = Color.Transparent;
-            pointsLabel.Location = new Point(469, 31);
-            pointsLabel.Name = "pointsLabel";
-            pointsLabel.Size = new Size(40, 15);
-            pointsLabel.TabIndex = 3;
-            pointsLabel.Text = "Points";
-            pointsLabel.TextAlign = ContentAlignment.MiddleCenter;
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("MV Boli", 24F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(23, 308);
+            label2.Name = "label2";
+            label2.Size = new Size(214, 41);
+            label2.TabIndex = 6;
+            label2.Text = "Meal Combos";
+            label2.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.Transparent;
+            label4.Font = new Font("MV Boli", 24F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(12, 540);
+            label4.Name = "label4";
+            label4.Size = new Size(261, 41);
+            label4.TabIndex = 7;
+            label4.Text = "Today's Bargains";
+            label4.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // seasonalSpecialImage1
+            // 
+            seasonalSpecialImage1.Location = new Point(23, 129);
+            seasonalSpecialImage1.Name = "seasonalSpecialImage1";
+            seasonalSpecialImage1.Size = new Size(140, 140);
+            seasonalSpecialImage1.TabIndex = 8;
+            seasonalSpecialImage1.Text = "button1";
+            seasonalSpecialImage1.UseVisualStyleBackColor = true;
+            // 
+            // seasonalSpecialImage2
+            // 
+            seasonalSpecialImage2.Location = new Point(196, 129);
+            seasonalSpecialImage2.Name = "seasonalSpecialImage2";
+            seasonalSpecialImage2.Size = new Size(140, 140);
+            seasonalSpecialImage2.TabIndex = 9;
+            seasonalSpecialImage2.Text = "button2";
+            seasonalSpecialImage2.UseVisualStyleBackColor = true;
+            // 
+            // seasonalSpecialImage3
+            // 
+            seasonalSpecialImage3.Location = new Point(369, 129);
+            seasonalSpecialImage3.Name = "seasonalSpecialImage3";
+            seasonalSpecialImage3.Size = new Size(140, 140);
+            seasonalSpecialImage3.TabIndex = 10;
+            seasonalSpecialImage3.Text = "button3";
+            seasonalSpecialImage3.UseVisualStyleBackColor = true;
+            // 
+            // mealComboImage3
+            // 
+            mealComboImage3.Location = new Point(369, 352);
+            mealComboImage3.Name = "mealComboImage3";
+            mealComboImage3.Size = new Size(140, 140);
+            mealComboImage3.TabIndex = 13;
+            mealComboImage3.Text = "button3";
+            mealComboImage3.UseVisualStyleBackColor = true;
+            // 
+            // mealComboImage2
+            // 
+            mealComboImage2.Location = new Point(196, 352);
+            mealComboImage2.Name = "mealComboImage2";
+            mealComboImage2.Size = new Size(140, 140);
+            mealComboImage2.TabIndex = 12;
+            mealComboImage2.Text = "button2";
+            mealComboImage2.UseVisualStyleBackColor = true;
+            // 
+            // mealComboImage1
+            // 
+            mealComboImage1.Location = new Point(23, 352);
+            mealComboImage1.Name = "mealComboImage1";
+            mealComboImage1.Size = new Size(140, 140);
+            mealComboImage1.TabIndex = 11;
+            mealComboImage1.Text = "button1";
+            mealComboImage1.UseVisualStyleBackColor = true;
+            // 
+            // todayBargainImage3
+            // 
+            todayBargainImage3.Location = new Point(369, 584);
+            todayBargainImage3.Name = "todayBargainImage3";
+            todayBargainImage3.Size = new Size(140, 140);
+            todayBargainImage3.TabIndex = 16;
+            todayBargainImage3.Text = "button3";
+            todayBargainImage3.UseVisualStyleBackColor = true;
+            // 
+            // todayBargainImage2
+            // 
+            todayBargainImage2.Location = new Point(196, 584);
+            todayBargainImage2.Name = "todayBargainImage2";
+            todayBargainImage2.Size = new Size(140, 140);
+            todayBargainImage2.TabIndex = 15;
+            todayBargainImage2.Text = "button2";
+            todayBargainImage2.UseVisualStyleBackColor = true;
+            // 
+            // todayBargainImage1
+            // 
+            todayBargainImage1.Location = new Point(23, 584);
+            todayBargainImage1.Name = "todayBargainImage1";
+            todayBargainImage1.Size = new Size(140, 140);
+            todayBargainImage1.TabIndex = 14;
+            todayBargainImage1.Text = "button1";
+            todayBargainImage1.UseVisualStyleBackColor = true;
             // 
             // orderTakingMenu
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Window;
             ClientSize = new Size(524, 921);
+            Controls.Add(todayBargainImage3);
+            Controls.Add(todayBargainImage2);
+            Controls.Add(todayBargainImage1);
+            Controls.Add(mealComboImage3);
+            Controls.Add(mealComboImage2);
+            Controls.Add(mealComboImage1);
+            Controls.Add(seasonalSpecialImage3);
+            Controls.Add(seasonalSpecialImage2);
+            Controls.Add(seasonalSpecialImage1);
+            Controls.Add(label4);
+            Controls.Add(label2);
+            Controls.Add(label3);
             Controls.Add(headerBackground);
             Name = "orderTakingMenu";
             Text = "Quickbite Order Taking Menu";
             headerBackground.ResumeLayout(false);
             headerBackground.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -103,6 +260,18 @@
         private Label pointsLabel;
         private Label pointsText;
         private Panel headerBackground;
-        private Button button1;
+        private Button seasonalSpecialImage1;
+        private Label label1;
+        private Label label3;
+        private Label label2;
+        private Label label4;
+        private Button seasonalSpecialImage2;
+        private Button seasonalSpecialImage3;
+        private Button mealComboImage3;
+        private Button mealComboImage2;
+        private Button mealComboImage1;
+        private Button todayBargainImage3;
+        private Button todayBargainImage2;
+        private Button todayBargainImage1;
     }
 }
