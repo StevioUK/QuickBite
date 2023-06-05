82d34866360327f4e52829e6099d1e27ac1384ce
Stevio <59091187+StevioUK@users.noreply.github.com>
2023-06-06 00:47:48 +0100

    changes to design
    Project scope changed

--- QuickBite/orderTakingMenu.Designer.cs
+++ QuickBite/orderTakingMenu.Designer.cs
@@ -46,7 +46,22 @@
             todayBargainImage3 = new Button();
             todayBargainImage2 = new Button();
             todayBargainImage1 = new Button();
+            seasonalFlowLayoutPanel = new FlowLayoutPanel();
+            seasonalSpecialImage4 = new Button();
+            mealFlowLayoutPanel = new FlowLayoutPanel();
+            mealComboImage4 = new Button();
+            bargainFlowLayoutPanel = new FlowLayoutPanel();
+            todayBargainImage4 = new Button();
+            seasonalSpecialTable = new TableLayoutPanel();
+            checkoutButton = new Button();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            totalCostLabel = new Label();
             headerBackground.SuspendLayout();
+            seasonalFlowLayoutPanel.SuspendLayout();
+            mealFlowLayoutPanel.SuspendLayout();
+            bargainFlowLayoutPanel.SuspendLayout();
+            seasonalSpecialTable.SuspendLayout();
+            tableLayoutPanel1.SuspendLayout();
             SuspendLayout();
             // 
             // headerBackground
@@ -59,15 +74,16 @@
             headerBackground.Dock = DockStyle.Top;
             headerBackground.Location = new Point(0, 0);
             headerBackground.Name = "headerBackground";
-            headerBackground.Size = new Size(524, 65);
+            headerBackground.Size = new Size(554, 65);
             headerBackground.TabIndex = 0;
             // 
             // label1
             // 
+            label1.Anchor = AnchorStyles.None;
             label1.AutoSize = true;
             label1.BackColor = Color.Transparent;
             label1.Font = new Font("MV Boli", 24F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.Location = new Point(94, 10);
+            label1.Location = new Point(111, 10);
             label1.Name = "label1";
             label1.Size = new Size(347, 41);
             label1.TabIndex = 4;
@@ -76,9 +92,10 @@
             // 
             // pointsLabel
             // 
+            pointsLabel.Anchor = AnchorStyles.None;
             pointsLabel.AutoSize = true;
             pointsLabel.BackColor = Color.Transparent;
-            pointsLabel.Location = new Point(469, 31);
+            pointsLabel.Location = new Point(506, 31);
             pointsLabel.Name = "pointsLabel";
             pointsLabel.Size = new Size(40, 15);
             pointsLabel.TabIndex = 3;
@@ -87,9 +104,10 @@
             // 
             // pointsText
             // 
+            pointsText.Anchor = AnchorStyles.None;
             pointsText.AutoSize = true;
             pointsText.BackColor = Color.Transparent;
-            pointsText.Location = new Point(469, 12);
+            pointsText.Location = new Point(506, 12);
             pointsText.Name = "pointsText";
             pointsText.Size = new Size(43, 15);
             pointsText.TabIndex = 2;
@@ -98,13 +116,14 @@
             // 
             // listViewButton
             // 
+            listViewButton.Anchor = AnchorStyles.None;
             listViewButton.BackColor = Color.Transparent;
             listViewButton.BackgroundImage = (Image)resources.GetObject("listViewButton.BackgroundImage");
             listViewButton.BackgroundImageLayout = ImageLayout.Stretch;
             listViewButton.FlatAppearance.BorderSize = 0;
             listViewButton.FlatStyle = FlatStyle.Flat;
             listViewButton.ForeColor = Color.Transparent;
-            listViewButton.Location = new Point(3, 3);
+            listViewButton.Location = new Point(2, 3);
             listViewButton.Name = "listViewButton";
             listViewButton.Size = new Size(60, 60);
             listViewButton.TabIndex = 1;
@@ -112,10 +131,11 @@
             // 
             // label3
             // 
+            label3.Anchor = AnchorStyles.None;
             label3.AutoSize = true;
             label3.BackColor = Color.Transparent;
             label3.Font = new Font("MV Boli", 24F, FontStyle.Regular, GraphicsUnit.Point);
-            label3.Location = new Point(12, 85);
+            label3.Location = new Point(141, 6);
             label3.Name = "label3";
             label3.Size = new Size(271, 41);
             label3.TabIndex = 5;
@@ -124,10 +144,11 @@
             // 
             // label2
             // 
+            label2.Anchor = AnchorStyles.None;
             label2.AutoSize = true;
             label2.BackColor = Color.Transparent;
             label2.Font = new Font("MV Boli", 24F, FontStyle.Regular, GraphicsUnit.Point);
-            label2.Location = new Point(23, 308);
+            label2.Location = new Point(170, 230);
             label2.Name = "label2";
             label2.Size = new Size(214, 41);
             label2.TabIndex = 6;
@@ -136,10 +157,11 @@
             // 
             // label4
             // 
+            label4.Anchor = AnchorStyles.None;
             label4.AutoSize = true;
             label4.BackColor = Color.Transparent;
             label4.Font = new Font("MV Boli", 24F, FontStyle.Regular, GraphicsUnit.Point);
-            label4.Location = new Point(12, 540);
+            label4.Location = new Point(146, 454);
             label4.Name = "label4";
             label4.Size = new Size(261, 41);
             label4.TabIndex = 7;
@@ -148,7 +170,9 @@
             // 
             // seasonalSpecialImage1
             // 
-            seasonalSpecialImage1.Location = new Point(23, 129);
+            seasonalSpecialImage1.Anchor = AnchorStyles.None;
+            seasonalSpecialImage1.AutoSize = true;
+            seasonalSpecialImage1.Location = new Point(3, 3);
             seasonalSpecialImage1.Name = "seasonalSpecialImage1";
             seasonalSpecialImage1.Size = new Size(140, 140);
             seasonalSpecialImage1.TabIndex = 8;
@@ -157,7 +181,9 @@
             // 
             // seasonalSpecialImage2
             // 
-            seasonalSpecialImage2.Location = new Point(196, 129);
+            seasonalSpecialImage2.Anchor = AnchorStyles.None;
+            seasonalSpecialImage2.AutoSize = true;
+            seasonalSpecialImage2.Location = new Point(149, 3);
             seasonalSpecialImage2.Name = "seasonalSpecialImage2";
             seasonalSpecialImage2.Size = new Size(140, 140);
             seasonalSpecialImage2.TabIndex = 9;
@@ -166,7 +192,9 @@
             // 
             // seasonalSpecialImage3
             // 
-            seasonalSpecialImage3.Location = new Point(369, 129);
+            seasonalSpecialImage3.Anchor = AnchorStyles.None;
+            seasonalSpecialImage3.AutoSize = true;
+            seasonalSpecialImage3.Location = new Point(295, 3);
             seasonalSpecialImage3.Name = "seasonalSpecialImage3";
             seasonalSpecialImage3.Size = new Size(140, 140);
             seasonalSpecialImage3.TabIndex = 10;
@@ -175,7 +203,7 @@
             // 
             // mealComboImage3
             // 
-            mealComboImage3.Location = new Point(369, 352);
+            mealComboImage3.Location = new Point(295, 3);
             mealComboImage3.Name = "mealComboImage3";
             mealComboImage3.Size = new Size(140, 140);
             mealComboImage3.TabIndex = 13;
@@ -184,7 +212,7 @@
             // 
             // mealComboImage2
             // 
-            mealComboImage2.Location = new Point(196, 352);
+            mealComboImage2.Location = new Point(149, 3);
             mealComboImage2.Name = "mealComboImage2";
             mealComboImage2.Size = new Size(140, 140);
             mealComboImage2.TabIndex = 12;
@@ -193,7 +221,7 @@
             // 
             // mealComboImage1
             // 
-            mealComboImage1.Location = new Point(23, 352);
+            mealComboImage1.Location = new Point(3, 3);
             mealComboImage1.Name = "mealComboImage1";
             mealComboImage1.Size = new Size(140, 140);
             mealComboImage1.TabIndex = 11;
@@ -202,7 +230,7 @@
             // 
             // todayBargainImage3
             // 
-            todayBargainImage3.Location = new Point(369, 584);
+            todayBargainImage3.Location = new Point(295, 3);
             todayBargainImage3.Name = "todayBargainImage3";
             todayBargainImage3.Size = new Size(140, 140);
             todayBargainImage3.TabIndex = 16;
@@ -211,7 +239,7 @@
             // 
             // todayBargainImage2
             // 
-            todayBargainImage2.Location = new Point(196, 584);
+            todayBargainImage2.Location = new Point(149, 3);
             todayBargainImage2.Name = "todayBargainImage2";
             todayBargainImage2.Size = new Size(140, 140);
             todayBargainImage2.TabIndex = 15;
@@ -220,38 +248,169 @@
             // 
             // todayBargainImage1
             // 
-            todayBargainImage1.Location = new Point(23, 584);
+            todayBargainImage1.Location = new Point(3, 3);
             todayBargainImage1.Name = "todayBargainImage1";
             todayBargainImage1.Size = new Size(140, 140);
             todayBargainImage1.TabIndex = 14;
             todayBargainImage1.Text = "button1";
             todayBargainImage1.UseVisualStyleBackColor = true;
             // 
+            // seasonalFlowLayoutPanel
+            // 
+            seasonalFlowLayoutPanel.AutoScroll = true;
+            seasonalFlowLayoutPanel.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            seasonalFlowLayoutPanel.Controls.Add(seasonalSpecialImage1);
+            seasonalFlowLayoutPanel.Controls.Add(seasonalSpecialImage2);
+            seasonalFlowLayoutPanel.Controls.Add(seasonalSpecialImage3);
+            seasonalFlowLayoutPanel.Controls.Add(seasonalSpecialImage4);
+            seasonalFlowLayoutPanel.Location = new Point(3, 56);
+            seasonalFlowLayoutPanel.MinimumSize = new Size(263, 94);
+            seasonalFlowLayoutPanel.Name = "seasonalFlowLayoutPanel";
+            seasonalFlowLayoutPanel.Size = new Size(548, 165);
+            seasonalFlowLayoutPanel.TabIndex = 17;
+            seasonalFlowLayoutPanel.WrapContents = false;
+            // 
+            // seasonalSpecialImage4
+            // 
+            seasonalSpecialImage4.Anchor = AnchorStyles.None;
+            seasonalSpecialImage4.AutoSize = true;
+            seasonalSpecialImage4.Location = new Point(441, 3);
+            seasonalSpecialImage4.Name = "seasonalSpecialImage4";
+            seasonalSpecialImage4.Size = new Size(140, 140);
+            seasonalSpecialImage4.TabIndex = 11;
+            seasonalSpecialImage4.Text = "button4";
+            seasonalSpecialImage4.UseVisualStyleBackColor = true;
+            // 
+            // mealFlowLayoutPanel
+            // 
+            mealFlowLayoutPanel.AutoScroll = true;
+            mealFlowLayoutPanel.Controls.Add(mealComboImage1);
+            mealFlowLayoutPanel.Controls.Add(mealComboImage2);
+            mealFlowLayoutPanel.Controls.Add(mealComboImage3);
+            mealFlowLayoutPanel.Controls.Add(mealComboImage4);
+            mealFlowLayoutPanel.Location = new Point(3, 280);
+            mealFlowLayoutPanel.Name = "mealFlowLayoutPanel";
+            mealFlowLayoutPanel.Size = new Size(548, 165);
+            mealFlowLayoutPanel.TabIndex = 18;
+            mealFlowLayoutPanel.WrapContents = false;
+            // 
+            // mealComboImage4
+            // 
+            mealComboImage4.Location = new Point(441, 3);
+            mealComboImage4.Name = "mealComboImage4";
+            mealComboImage4.Size = new Size(140, 140);
+            mealComboImage4.TabIndex = 14;
+            mealComboImage4.Text = "button4";
+            mealComboImage4.UseVisualStyleBackColor = true;
+            // 
+            // bargainFlowLayoutPanel
+            // 
+            bargainFlowLayoutPanel.AutoScroll = true;
+            bargainFlowLayoutPanel.Controls.Add(todayBargainImage1);
+            bargainFlowLayoutPanel.Controls.Add(todayBargainImage2);
+            bargainFlowLayoutPanel.Controls.Add(todayBargainImage3);
+            bargainFlowLayoutPanel.Controls.Add(todayBargainImage4);
+            bargainFlowLayoutPanel.Location = new Point(3, 504);
+            bargainFlowLayoutPanel.Name = "bargainFlowLayoutPanel";
+            bargainFlowLayoutPanel.Size = new Size(548, 165);
+            bargainFlowLayoutPanel.TabIndex = 19;
+            bargainFlowLayoutPanel.WrapContents = false;
+            // 
+            // todayBargainImage4
+            // 
+            todayBargainImage4.Location = new Point(441, 3);
+            todayBargainImage4.Name = "todayBargainImage4";
+            todayBargainImage4.Size = new Size(140, 140);
+            todayBargainImage4.TabIndex = 17;
+            todayBargainImage4.Text = "button4";
+            todayBargainImage4.UseVisualStyleBackColor = true;
+            // 
+            // seasonalSpecialTable
+            // 
+            seasonalSpecialTable.Anchor = AnchorStyles.None;
+            seasonalSpecialTable.AutoScroll = true;
+            seasonalSpecialTable.ColumnCount = 1;
+            seasonalSpecialTable.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
+            seasonalSpecialTable.Controls.Add(bargainFlowLayoutPanel, 0, 5);
+            seasonalSpecialTable.Controls.Add(mealFlowLayoutPanel, 0, 3);
+            seasonalSpecialTable.Controls.Add(label3, 0, 0);
+            seasonalSpecialTable.Controls.Add(label4, 0, 4);
+            seasonalSpecialTable.Controls.Add(label2, 0, 2);
+            seasonalSpecialTable.Controls.Add(seasonalFlowLayoutPanel, 0, 1);
+            seasonalSpecialTable.Location = new Point(0, 62);
+            seasonalSpecialTable.Name = "seasonalSpecialTable";
+            seasonalSpecialTable.RowCount = 6;
+            seasonalSpecialTable.RowStyles.Add(new RowStyle(SizeType.Percent, 7.97232676F));
+            seasonalSpecialTable.RowStyles.Add(new RowStyle(SizeType.Percent, 25.36136F));
+            seasonalSpecialTable.RowStyles.Add(new RowStyle(SizeType.Percent, 7.9717927F));
+            seasonalSpecialTable.RowStyles.Add(new RowStyle(SizeType.Percent, 25.36136F));
+            seasonalSpecialTable.RowStyles.Add(new RowStyle(SizeType.Percent, 7.9717927F));
+            seasonalSpecialTable.RowStyles.Add(new RowStyle(SizeType.Percent, 25.36136F));
+            seasonalSpecialTable.Size = new Size(554, 677);
+            seasonalSpecialTable.TabIndex = 20;
+            // 
+            // checkoutButton
+            // 
+            checkoutButton.AutoSize = true;
+            checkoutButton.Font = new Font("Leelawadee UI", 36F, FontStyle.Regular, GraphicsUnit.Point);
+            checkoutButton.Location = new Point(3, 3);
+            checkoutButton.Name = "checkoutButton";
+            checkoutButton.Size = new Size(432, 94);
+            checkoutButton.TabIndex = 12;
+            checkoutButton.Text = "Checkout Now!";
+            checkoutButton.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.Anchor = AnchorStyles.None;
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 113F));
+            tableLayoutPanel1.Controls.Add(totalCostLabel, 1, 0);
+            tableLayoutPanel1.Controls.Add(checkoutButton, 0, 0);
+            tableLayoutPanel1.Location = new Point(0, 745);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 1;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Size = new Size(554, 100);
+            tableLayoutPanel1.TabIndex = 21;
+            // 
+            // totalCostLabel
+            // 
+            totalCostLabel.Anchor = AnchorStyles.None;
+            totalCostLabel.AutoSize = true;
+            totalCostLabel.BackColor = Color.Transparent;
+            totalCostLabel.Font = new Font("MV Boli", 24F, FontStyle.Regular, GraphicsUnit.Point);
+            totalCostLabel.Location = new Point(479, 29);
+            totalCostLabel.Name = "totalCostLabel";
+            totalCostLabel.Size = new Size(36, 41);
+            totalCostLabel.TabIndex = 22;
+            totalCostLabel.Text = "$";
+            totalCostLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
             // orderTakingMenu
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.Window;
-            ClientSize = new Size(524, 921);
-            Controls.Add(todayBargainImage3);
-            Controls.Add(todayBargainImage2);
-            Controls.Add(todayBargainImage1);
-            Controls.Add(mealComboImage3);
-            Controls.Add(mealComboImage2);
-            Controls.Add(mealComboImage1);
-            Controls.Add(seasonalSpecialImage3);
-            Controls.Add(seasonalSpecialImage2);
-            Controls.Add(seasonalSpecialImage1);
-            Controls.Add(label4);
-            Controls.Add(label2);
-            Controls.Add(label3);
+            ClientSize = new Size(554, 921);
+            Controls.Add(tableLayoutPanel1);
+            Controls.Add(seasonalSpecialTable);
             Controls.Add(headerBackground);
+            MinimumSize = new Size(570, 960);
             Name = "orderTakingMenu";
             Text = "Quickbite Order Taking Menu";
             headerBackground.ResumeLayout(false);
             headerBackground.PerformLayout();
+            seasonalFlowLayoutPanel.ResumeLayout(false);
+            seasonalFlowLayoutPanel.PerformLayout();
+            mealFlowLayoutPanel.ResumeLayout(false);
+            bargainFlowLayoutPanel.ResumeLayout(false);
+            seasonalSpecialTable.ResumeLayout(false);
+            seasonalSpecialTable.PerformLayout();
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
@@ -273,5 +432,15 @@
         private Button todayBargainImage3;
         private Button todayBargainImage2;
         private Button todayBargainImage1;
+        private FlowLayoutPanel seasonalFlowLayoutPanel;
+        private Button seasonalSpecialImage4;
+        private FlowLayoutPanel mealFlowLayoutPanel;
+        private FlowLayoutPanel bargainFlowLayoutPanel;
+        private Button mealComboImage4;
+        private Button todayBargainImage4;
+        private TableLayoutPanel seasonalSpecialTable;
+        private Button checkoutButton;
+        private TableLayoutPanel tableLayoutPanel1;
+        private Label totalCostLabel;
     }
 }
